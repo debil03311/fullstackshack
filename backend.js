e2fecf91dea424fe5cb38587f94ef3706cec02e9
hosma <andrei.hent@yahoo.com>
2022-02-10 13:40:08 +0200

    Initial commit

--- backend.js
+++ backend.js
@@ -8,13 +8,18 @@ app.set('view engine', 'ejs');  // specify the rendering engine
 app.use(express.static(__dirname + '/views'));      // use the current directory + additional ones for dependencies within files
 
 app.get('/front_page', (req, res) => {
-    //res.render('front_page');     // this uses EJS to render the page
+    // this uses EJS to render the page
+    //res.render('front_page');
+
     // this just sends the file directly
-    res.sendFile(`${__dirname}\\views\\front_page.html`);   // __dirname is awesome, it gets the current directory
+    // __dirname is awesome, it gets the current directory
+    res.sendFile(`${__dirname}\\views\\front_page.html`);
 });
 
 app.post('/front_page', (req, res) => {
-    const d = new Date();   // gets current time, in millisecond since 1/01/1970, so use the time formatting functions below 
+    // gets current time, in millisecond since 1/01/1970,
+    // so use the time formatting functions below
+    const d = new Date();
 
     if ( req.body.comments != "" ) {    // only run if string is not empty
         console.log(`Message: ${req.body.comments} | From: ${req.ip} | On: ${d.getUTCDate()}/${d.getUTCMonth()}/${d.getUTCFullYear()} | At: ${d.getUTCHours()}:${d.getUTCMinutes()} UTC`);
